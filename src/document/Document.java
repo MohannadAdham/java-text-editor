e7243ad5ecba5f4fdb1035ccd3a445cfbf98358d
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-14 08:37:49 +0100

    Module 2 - correct the formula in the method getFleschScore in Document class.

--- src/document/Document.java
+++ src/document/Document.java
@@ -153,9 +153,9 @@ public abstract class Document {
 	{
 	    // TODO: You will play with this method in week 1, and 
 		// then implement it in week 2
-		double fleshScore = 206.835 - 1.015 * (getNumWords() / getNumSentences()) -
-				84.6 * (getNumSyllables() / getNumWords());
-		
+		double fleshScore = 206.835 - 1.015 * ((double)getNumWords() / getNumSentences()) -
+				84.6 * ((double)getNumSyllables() / getNumWords());
+		System.out.println((double)getNumWords() / getNumSentences());
 	    return fleshScore;
 	}
 	
